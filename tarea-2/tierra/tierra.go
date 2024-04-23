9eb4c4cb52e5cdcf45e1dd5a7cfb51827717e445
ElMaikina <mikesoto890@gmail.com>
2024-04-22 20:26:44 -0400

    devoler el tierra.go como estaba

--- tarea-2/tierra/tierra.go
+++ tarea-2/tierra/tierra.go
@@ -12,29 +12,6 @@ type myGemaEstrategicaServer struct {
 	pb.UnimplementedGemaEstrategicaServer
 }
 
-type Tierra struct {
-	AT        int // Cantidad de munición AT en inventario
-	MP        int // Cantidad de munición MP en inventario
-	MaxAT     int // Máximo de munición AT que se puede almacenar
-	MaxMP     int // Máximo de munición MP que se puede almacenar
-	grpcAddr  string
-}
-
-func (t *Tierra) GenerarMunicion() {
-	for {
-		time.Sleep(5 * time.Second)
-		t.AT += 10 // Genera 10 AT cada 5 segundos
-		t.MP += 5  // Genera 5 MP cada 5 segundos
-		// Verifica si el inventario supera el límite máximo y ajusta si es necesario
-		if t.AT > t.MaxAT {
-			t.AT = t.MaxAT
-		}
-		if t.MP > t.MaxMP {
-			t.MP = t.MaxMP
-		}
-	}
-}
-
 func (s myGemaEstrategicaServer) SolicitarM(context.Context, *pb.Solicitud) (*pb.Respuesta, error) {
 	log.Printf("Llego un mensaje nuevo!")
 	return &pb.Respuesta{Rpta: 1}, nil
@@ -42,6 +19,9 @@ func (s myGemaEstrategicaServer) SolicitarM(context.Context, *pb.Solicitud) (*pb
 
 func main() {
 	lis, err := net.Listen("tcp", ":8000")
+
+	AT := 0
+	MP := 0
 
 	if err != nil {
 		log.Fatalf("ERROR: %s", err)
@@ -56,7 +36,4 @@ func main() {
 	if err != nil {
 		log.Fatalf("ERROR: %s", err)
 	}
-
-	Tierra tierra;
-	GenerarMunicion()
 }
