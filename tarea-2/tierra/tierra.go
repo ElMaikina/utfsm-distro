9cc793a92b86159731add9b6fe7d1fba3244ab46
ElMaikina <mikesoto890@gmail.com>
2024-04-22 21:39:27 -0400

    tierra completamente implementada

--- tarea-2/tierra/tierra.go
+++ tarea-2/tierra/tierra.go
@@ -4,29 +4,76 @@ import (
 	"context"
 	"log"
 	"net"
+	"fmt"
+	"time"
+	"sync"
 	pb "main/proto"
 	"google.golang.org/grpc"
 )
+
+var maxAT int32 = 50
+var maxMP int32 = 20
+var AT int32 = 0
+var MP int32 = 0
 
 type myGemaEstrategicaServer struct {
 	pb.UnimplementedGemaEstrategicaServer
 }
 
-func (s myGemaEstrategicaServer) SolicitarM(context.Context, *pb.Solicitud) (*pb.Respuesta, error) {
-	log.Printf("Llego un mensaje nuevo!")
+func (s myGemaEstrategicaServer) SolicitarM(con context.Context, sol *pb.Solicitud) (*pb.Respuesta, error) {
+	if (sol.AT > AT) {
+		fmt.Printf("Recepcion de solicitud desde equipo %d, %d AT y %d MP -- DENEGADA -- AT EN SISTEMA: %d ; MP EN SISTEMA: %d\n", sol.ID, sol.AT, sol.MP, AT, MP)
+		return &pb.Respuesta{Rpta: 0}, nil
+	}
+	if (sol.MP > MP) {
+		fmt.Printf("Recepcion de solicitud desde equipo %d, %d AT y %d MP -- DENEGADA -- AT EN SISTEMA: %d ; MP EN SISTEMA: %d\n", sol.ID, sol.AT, sol.MP, AT, MP)
+		return &pb.Respuesta{Rpta: 0}, nil
+	}
+	AT -= sol.AT
+	MP -= sol.MP
+	fmt.Printf("Recepcion de solicitud desde equipo %d, %d AT y %d MP -- APROBADA -- AT EN SISTEMA: %d ; MP EN SISTEMA: %d\n", sol.ID, sol.AT, sol.MP, AT, MP)
 	return &pb.Respuesta{Rpta: 1}, nil
 }
 
-func main() {
-	lis, err := net.Listen("tcp", ":8000")
+// Funcion que produce misiles cada 1 segundo
+func producirMisiles(wg *sync.WaitGroup) {
+    defer wg.Done()
+	
+    for 
+	{
+		// Produce AT, si se pasa queda en su maximo valor
+		if (AT < maxAT) {
+			AT += 10
+		} 
+		if (AT >= maxAT) {
+			AT = maxAT
+		}
+		
+		// Produce MP, si se pasa queda en su maximo valor
+		if (MP < maxMP) {
+			MP += 5
+		} 
+		if (MP >= maxMP) {
+			MP = maxMP
+		}
 
-	AT := 0
-	MP := 0
+		// Pausa de 1 segundo
+        time.Sleep(1 * time.Second)
+    }
+}
+
+// Esucha las llamadas de gRPC en un proceso separado
+func escucharLlamadas(wg *sync.WaitGroup) {
+	defer wg.Done()
+	
+	// Abre y escucha al puerto
+	lis, err := net.Listen("tcp", ":8000")
 
 	if err != nil {
 		log.Fatalf("ERROR: %s", err)
 	}
 
+	// Se crea un servidor gRPC
 	serverRegister := grpc.NewServer()
 	service := &myGemaEstrategicaServer{}
 	pb.RegisterGemaEstrategicaServer(serverRegister, service)
@@ -36,4 +83,17 @@ func main() {
 	if err != nil {
 		log.Fatalf("ERROR: %s", err)
 	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+    wg.Add(2)
+	
+	// Escucha al servidor y produce misiles simultaneamente
+	go escucharLlamadas(&wg)
+	go producirMisiles(&wg)
+
+	// Esperamos a que ambas GoRoutines finalicen
+	wg.Wait()
 }
